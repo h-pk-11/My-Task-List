6a198dc1fcfd8a2e8eb3dda1e0ea0915a8f9d307
h-pk-11-windows <105375102+h-pk-11@users.noreply.github.com>
2023-12-27 17:24:51 +0700

    update build/ folder

--- src/assets/javascripts/date.js
+++
@@ -1,5 +0,0 @@
-import { format } from "date-fns";
-
-export default function formatDate(date, form) {
-  return format(date, form);
-}
--- src/assets/javascripts/index.js
+++
@@ -1,6 +0,0 @@
-import "../style/base.css";
-import "../style/normalize.css";
-import "../style/style.css";
-import viewController from "./UI";
-
-viewController.start();
